98845bd04c52ed49d48d3b9954963483a71713fa
Matthew <mwalton2@oberlin.edu>
2022-01-23 06:33:50 -0500

    Eliminated proper handling of user input. SQL Searching works well with 2 test cases

--- searchForm.php
+++ searchForm.php
@@ -6,16 +6,24 @@ $dbName 	= "mwalton2";
 $user		= "mwalton2";
 $pw	 	= "Oberlin@123";
 
-function PrintPage($body, $year) {
-  print("<!DOCTYPE html>\n");
-  print("<html>\n<head>\n<title>This is the php script that runs!</title>\n");
-  print("</head>\n<body>\n");
-  /*
-  print("<h1>Top movies from $year</h1>\n");
-  */
-  print("<div class='formOutput'>$body\n</div>\n");
-  
-  print("</body>\n</html>\n");
+function PrintPage($result_body, $query) {
+  print("<!DOCTYPE html>\n
+          <html>\n
+            <head>\n
+              <link rel='stylesheet' href='resultsStyle.css'>
+              <title>Search Results</title>\n
+            </head>\n
+            <h1>\n
+              Search Results <br>
+            </h1>\n
+            <h2>\n
+              $query <br>
+              $result_body <br>
+            </h2>\n
+            <h3>\n
+              $result_body <br>
+            </h3>\n
+          </html>\n");
 }
 
 
@@ -23,50 +31,50 @@ try {
     $firstInput = $_POST["firstTextInput"];
     $secondInput = $_POST["secondTextInput"];
     $thirdInput = $_POST["thirdTextInput"];
-
-    echo "You wanted: $firstInput where $secondInput is $thirdInput";
-
-  $body = "<table><tr><th>CharID</th><th>charName</th><th>Gender</th><th>eyeColor</th></tr>";
-
-  $conn = new PDO("mysql:host=$serverName;dbname=$dbName", 
-		  $user, $pw);
-  // set the PDO error mode to exception
-  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-  // Test statement
-  $stmt = $conn->prepare('select * from Characters where Gender = :thirdInput;');
-  
-  
-  // given statement:
-  //$stmt = $conn->prepare('select title, avg(rating) as avg_rating from movies natural join ratings WHERE YEAR = :year group by title order by avg_rating desc limit 20;');
-                          
-  $year = 1995;
-  $stmt->execute( array(':thirdInput' => $thirdInput) );
-
-  // Given statement
-  foreach($stmt->fetchAll(PDO::FETCH_ASSOC) as $key =>$val ) { 
-    $body .= "<tr><td>$key</td><td>" . 
-        $val['CharID'] . 
-	    "</td><td>" .
-        $val['charName'] . 
-	    "</td><td>" .
-        $val['Gender'] . 
-	    "</td><td>" .
-        $val['eyeColor'] . 
-        "</td></tr>\n";
-    }
-  
-
-  $body .= "</table>\n";
-
-  PrintPage($body, $year);
-
 }
-
 catch(PDOException $e) {
   PrintPage("Connection failed: " . $e->getMessage(), "Unknown");
 }
 
+  $result_body = "<table><tr><th>CharID</th><th>charName</th><th>Gender</th><th>eyeColor</th></tr>";
+
+$conn = new PDO("mysql:host=$serverName;dbname=$dbName", 
+    $user, $pw);
+// set the PDO error mode to exception
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$query = 'select * from Characters where ' . $secondInput . ' = \'' . $thirdInput . '\';';
+$stmt = $conn->prepare($query);
+$stmt->execute();
+
+foreach($stmt->fetchAll(PDO::FETCH_ASSOC) as $key => $val ) { 
+  $result_body .= "<td>" . 
+      $val['CharID'] . 
+      "</td><td>" .
+      $val['charName'] . 
+      "</td><td>" .
+      $val['Gender'] . 
+      "</td><td>" . 
+      $val['eyeColor'] . 
+      "</td></tr>\n";
+}
+$result_body .= "</table>\n";
+
+// Given statement
+foreach($stmt->fetchAll(PDO::FETCH_ASSOC) as $key =>$val ) { 
+  $result_body .= "<tr><td>$key</td><td>" . 
+      $val['CharID'] . 
+    "</td><td>" .
+      $val['charName'] . 
+    "</td><td>" .
+      $val['Gender'] . 
+    "</td><td>" .
+      $val['eyeColor'] . 
+      "</td></tr>\n";
+  }
+$result_body .= "</table>\n";
+PrintPage($result_body, $query);
 
 $conn = null;
+
 ?>
